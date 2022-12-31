c7bc05a972a5b27e5dd4da42d31fe50c661d169d
Thanhy Mather <tmather@vmware.com>
2022-12-31 09:15:08 -0800

    Initial Commit

---
+++ Marvin.IDP/HostingExtensions.cs
@@ -0,0 +1,70 @@
+using Marvin.IDP.DbContexts;
+using Marvin.IDP.Services;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace Marvin.IDP;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // uncomment if you want to add a UI
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddScoped<IPasswordHasher<Entities.User>, PasswordHasher<Entities.User>>();
+
+        builder.Services.AddScoped<ILocalUserService, LocalUserService>();
+
+        builder.Services.AddDbContext<IdentityDbContext>(options =>
+        {
+            options.UseSqlite(
+                builder.Configuration.GetConnectionString("MarvinIdentityDBConnectionString"));
+        });
+
+        builder.Services.AddIdentityServer(options =>
+            {
+                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddProfileService<LocalUserProfileService>()
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryApiResources(Config.ApiResources)
+            .AddInMemoryClients(Config.Clients);
+
+        builder.Services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                                       | ForwardedHeaders.XForwardedProto;
+        });
+
+        return builder.Build();
+    }
+    
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseForwardedHeaders();
+
+        app.UseSerilogRequestLogging();
+    
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // uncomment if you want to add a UI
+        app.UseStaticFiles();
+        app.UseRouting();
+            
+        app.UseIdentityServer();
+
+        // uncomment if you want to add a UI
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
+
+        return app;
+    }
+}
